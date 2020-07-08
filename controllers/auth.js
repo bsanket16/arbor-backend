9a9b65fcd6749912867d77b3538c67480975ffa8
sankt <bsankt16@gmail.com>
2020-07-08 13:20:38 +0530

    config authentication reply

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,22 @@
-exports.signup = (req, res, next) => {
-    console.log('REQ BODY', req.body)
-    res.json({
-        message: 'signup works!'
+const User = require('../models/user')
+
+exports.signup = (req, res) => {
+    const user = new User(req.body)
+    user.save((err, user) => {
+        if(err){
+            return res.status(400).json({
+                err: 'Not able to store user in database'
+            })
+        }
+        res.json({
+            name : user.name,
+            email : user.email,
+            id : user._id
+        })
     })
 }
 
-exports.logout = (req, res, next) => {
+exports.logout = (req, res) => {
     res.json({
         message: 'logout works!'
     })
--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose')
 const crypto = require('crypto')
-const uuidv1 = require('uuid/v1')
+const uuidv1 = require('uuidv1')
 
-var userSchema = new mongoose.Schema({
-
+var userSchema = new mongoose.Schema(
+{   
     name: {
         type: String,
         required: true,

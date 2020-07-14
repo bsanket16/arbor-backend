4986be92b6ec89fc117c40f469816d7214110643
sankt <bsankt16@gmail.com>
2020-07-14 21:04:31 +0530

    added: userPurchaseList

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const Order = require('../models/order')
 
 exports.getUserById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -40,4 +41,17 @@ exports.updateUser = (req, res) => {
             res.json(user)
         }
     )
+}
+
+exports.userPurchaseList = (req, res) => {
+    Order.find({user: req.profile._id})
+    .populate('user', '_id username')
+    .exec((err, order) => {
+        if(err){
+            return res.status(400).json({
+                error : 'No Orders'
+            })
+        }
+        return res.json(order)
+    })
 }
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth')
-const {getUserById, getUser, updateUser} = require('../controllers/user')
+const {getUserById, getUser, updateUser, userPurchaseList} = require('../controllers/user')
 
 router.param('userId', getUserById)
 router.get('/user/:userId', isSignedIn, isAuthenticated, getUser)
 router.put('/user/:userId', isSignedIn, isAuthenticated, updateUser)    
+router.get('/orders/user/:userId', isSignedIn, isAuthenticated, userPurchaseList)    
 
 module.exports = router

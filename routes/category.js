e5d87cd41cd97ff188102b049e058964fcbeebca
sankt <bsankt16@gmail.com>
2020-07-15 20:31:25 +0530

    middleware: getAllCategories | getCategory

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const {isAdmin, isAuthenticated, isSignedIn} = require('../controllers/auth')
 const {getUserById} = require('../controllers/user')
-const {getCategoryById, createCategory} = require('../controllers/category')
+const {getCategoryById, createCategory, getAllCategories, getCategory} = require('../controllers/category')
 
 //params
 router.param('userId', getUserById)
@@ -11,5 +11,8 @@ router.param('categoryId', getCategoryById)
 
 //routes
 router.post('/category/create/:userId', isSignedIn, isAuthenticated, isAdmin, createCategory)
+
+router.get('/categories', getAllCategories )
+router.get('/category/:categoryId', getCategory )
 
 module.exports = router

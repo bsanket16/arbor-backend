d3ca596c62d47c13d88d2ff995b5e195e838140b
sankt <bsankt16@gmail.com>
2020-07-15 20:18:02 +0530

    middleware: createCategory

--- routes/category.js
+++ routes/category.js
@@ -3,9 +3,13 @@ const router = express.Router()
 
 const {isAdmin, isAuthenticated, isSignedIn} = require('../controllers/auth')
 const {getUserById} = require('../controllers/user')
-const {getCategoryById} = require('../controllers/category')
+const {getCategoryById, createCategory} = require('../controllers/category')
 
+//params
 router.param('userId', getUserById)
 router.param('categoryId', getCategoryById)
+
+//routes
+router.post('/category/create/:userId', isSignedIn, isAuthenticated, isAdmin, createCategory)
 
 module.exports = router

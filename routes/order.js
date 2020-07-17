0bf8c8374928de53b56957b895ce3fef390b5363
sankt <bsankt16@gmail.com>
2020-07-17 15:33:44 +0530

    added : create order

--- routes/order.js
+++ routes/order.js
@@ -4,8 +4,13 @@ const router = express.Router()
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth')
 const { getUserById, pushOrderInPurchaseList } = require('../controllers/user')
 const { updateStock } = require('../controllers/product')
-const { getOrderById } = require('../controllers/order')
+const { getOrderById, createOrder } = require('../controllers/order')
 
 //params
 router.param('userId', getUserById)
 router.param('orderId', getOrderById)
+
+//routes
+
+//create
+router.post('/order/create/:userId', isSignedIn, isAuthenticated, pushOrderInPurchaseList, updateStock, createOrder)

5b8e129e266babc72d17fab996f68c9331eee21b
sankt <bsankt16@gmail.com>
2020-07-17 15:53:39 +0530

    added : get order status

--- controllers/order.js
+++ controllers/order.js
@@ -1,4 +1,4 @@
-const { Order, ProductCart } = require('../models/order')
+const { Order } = require('../models/order')
 
 exports.getOrderById = (req, res, next, id) => {
     Order.findById(id)
@@ -38,4 +38,23 @@ exports.getAllOrders = (req, res) => {
         }
         res.json(order)
     })
-}   
+}
+
+exports.getOrderStatus = (req, res) => {
+    res.json(Order.schema.path('status').enumValues)
+}
+
+exports.updateStatus = (req, res) => {
+    Order.update(
+        {_id : req.body.orderId},
+        {$set : {status: req.body.status}},
+        (err, order) => {
+            if(err){
+                return res.status(400).json({
+                    error : 'Cannot update order status'
+                })
+            }
+            res.json(order)
+        }
+    )
+}
--- routes/order.js
+++ routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth')
 const { getUserById, pushOrderInPurchaseList } = require('../controllers/user')
 const { updateStock } = require('../controllers/product')
-const { getOrderById, createOrder, getAllOrders } = require('../controllers/order')
+const { getOrderById, createOrder, getAllOrders, getOrderStatus, updateStatus } = require('../controllers/order')
 
 //params
 router.param('userId', getUserById)
@@ -17,3 +17,7 @@ router.post('/order/create/:userId', isSignedIn, isAuthenticated, pushOrderInPur
 
 //read
 router.get('/order/all/:userId', isSignedIn, isAuthenticated, isAdmin, getAllOrders )
+
+//status
+router.get('/order/status/:userId', isSignedIn, isAuthenticated, isAdmin, getOrderStatus )
+router.put('/order/:orderId/status/:userId', isSignedIn, isAuthenticated, isAdmin, updateStatus)   

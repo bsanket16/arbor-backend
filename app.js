3e365cabba4b101f1e473728a0e52790cb30b99f
sankt <bsankt16@gmail.com>
2020-07-17 15:28:22 +0530

    added : get order by id

--- app.js
+++ app.js
@@ -17,11 +17,13 @@ const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
+const orderRoutes = require('./routes/order')
 
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
 app.use('/api', categoryRoutes)
 app.use('/api', productRoutes)
+app.use('/api', orderRoutes)
 
 //db connection
 mongoose.connect( process.env.DATABASE, {

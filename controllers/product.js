207cfd6d31e56d78c11b645f316da589001a13f1
sankt <bsankt16@gmail.com>
2020-07-17 14:52:04 +0530

    added : product stock update

--- controllers/product.js
+++ controllers/product.js
@@ -145,3 +145,23 @@ exports.getAllProducts = (req, res) => {
         res.json(products)
     })
 }
+
+exports.updateStock = (req, res) => {
+    let myOperations = req.body.order.products.map(prod => {
+        return {
+            updateOne: {
+                filter: { _id: prod._id},
+                update: {$inc: {stock: -prod.count, sold: +prod.count}}
+            }
+        }
+    })
+
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if(err){
+            return res.status(400).json({
+                error : 'Bulk operation failed'
+            })
+        }
+        res.json(products)
+    })
+}   
